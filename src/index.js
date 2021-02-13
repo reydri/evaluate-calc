c499eb963bc0ef266abfe342b72567e27f533775
Reyhan Alkadri <reyhanalkadri@gmail.com>
2021-02-13 10:36:29 +0700

    responden

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 // todo
+var value1 = 15
+var value2 = 5
+
+function tambahAngka(calTambah){
+    calTambah = value1 + value2;
+    return calTambah;
+}
+
+console.log(tambahAngka("calTambah"));
+
+function kurangAngka(calKurang){
+    calKurang = value1 - value2;
+    return calKurang;
+}
+
+console.log(kurangAngka("calKurang"));
+
+function kaliAngka(calKali){
+    calKali = value1 * value2;
+    return calKali;
+}
+
+console.log(kaliAngka("calKali"));
+
+function bagiAngka(calBagi){
+    calBagi = value1 / value2;
+    return calBagi;
+}
+
+console.log(bagiAngka("calBagi"));
